4df0bf383a76ea69e317e804704082edf2242fc3
saram1sra <saram1@andrew.cmu.edu>
2019-07-24 15:01:06 -0400

    cleanup

--- unified_utils/tangentialDistortion.m
+++
@@ -1,9 +0,0 @@
-function tangentialDistortion (p1, p2)
-    max = 10;
-    [x, y] = meshgrid(-max:.5:max);
-    r = x.^2 + y.^2;
-    xy = x.*y*2;
-    figure;
-    quiver(x, y, xy*p1 + (r+2*x.^2)*p2, xy*p2  + (r+2*y.^2)*p1, 0);
-    axis("square");
-end
