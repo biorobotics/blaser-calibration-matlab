9c9053f60bf01fe1dc6da4f72bfcaacaa9e727de
harveybia <harvey.shi@yahoo.com>
2019-11-06 03:37:09 -0500

    made test_laser a function and smarter

--- test_laser.m
+++ test_laser.m
@@ -1,28 +1,46 @@
-addpath('unified_utils');
-addpath('blaser_data/640_unified');
-%addpath('/home/haowensh/Projects/blaser/calibration/laser_embedded')
-clear
+function [] = test_laser(calibDataPath)
+
+%% Load dataset
 close all
+addpath('unified_utils');
 
+if ~exist('calibDataPath', 'var')
+    % Default calibration dataset
+    calibDataPath = './blaser_data/640_unified';
+end
 
-n_val = 24;
+f_listing = dir(fullfile(calibDataPath, '*.png'));
+n_val = numel(f_listing);
+imgnames = repmat({''}, 1, n_val);
+for ii = 1:n_val
+    imgnames{ii} = f_listing(ii).name;
+end
+
 minLaserStripePts = 70;
 
 A = cell(n_val, 3);
 
+fprintf("------------------------\n");
+fprintf("Calibration dataset:\n    %s\n", calibDataPath);
+fprintf("Dataset size:\n    %d\n", n_val);
+
+% Note: this test assumes image is undistorted!
+% This test is intended for testing stripe extraction pipeline only.
+
+%% Laser test
 figure;
 for ii = 1:n_val
-      fprintf('Image %d\n', ii);
-      fname = append("im", int2str(ii),".png");
-      I = imread(fname);
-      
+      fname = imgnames{ii};
+      fprintf('Image %s\n', fname);
+      I = imread(fullfile(calibDataPath, fname));
+
       imshow(I);
       hold on;
       drawnow;
-    
+
     laser_pixels = find_laser_new(I);
-    %disp(size(laser_pixels));
-    
+    plot(laser_pixels(:,1), laser_pixels(:,2), 'r.');
+
     if numel(laser_pixels) > 1
         coeffs = polyfit(laser_pixels(:,1), laser_pixels(:,2), 1);
         dists = abs(polyval(coeffs, laser_pixels(:,1)) - laser_pixels(:,2));
@@ -43,9 +61,11 @@ for ii = 1:n_val
             plot(X, fline, '--', 'color', [0 1 1])
             hold off
             drawnow;
-            waitforbuttonpress;
 
             A{ii,3} = lpts;
         end
+
+        waitforbuttonpress;
     end
+        
 end
